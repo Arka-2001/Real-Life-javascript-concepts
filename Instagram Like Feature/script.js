28a706613f9ba97b211a3df2dba1e9a6e59e7d12
Arka Das <arkadas20102001@gmail.com>
2024-09-11 16:55:37 +0530

    Instagram Like Feature

---
+++ Instagram Like Feature/script.js
@@ -0,0 +1,18 @@
+
+
+const con=document.querySelector(".container")
+const loveicon=document.querySelector("i")
+
+con.addEventListener("dblclick",function(){
+    console.log("hello");
+    loveicon.style.transform="translate(-50%,-50%) scale(1)"
+    loveicon.style.opacity=0.8
+    setTimeout(function(){
+        loveicon.style.opacity=0;
+    },1000)
+    setTimeout(function(){
+        loveicon.style.transform="translate(-50%,-50%) scale(0)"
+    },1200)
+})
+
+
