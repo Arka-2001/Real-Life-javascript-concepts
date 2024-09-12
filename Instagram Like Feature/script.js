62fbc8e3256cc66516b71bf2eef7fc6db2549067
Arka Das <arkadas20102001@gmail.com>
2024-09-12 17:59:15 +0530

    Multiple images hover projet added

--- Instagram Like Feature/script.js
+++ Instagram Like Feature/script.js
@@ -4,7 +4,7 @@ const con=document.querySelector(".container")
 const loveicon=document.querySelector("i")
 
 con.addEventListener("dblclick",function(){
-    console.log("hello");
+    // console.log("hello");
     loveicon.style.transform="translate(-50%,-50%) scale(1)"
     loveicon.style.opacity=0.8
     setTimeout(function(){
